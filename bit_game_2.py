8a183b327992fadd66182d9984cf4f432afd12c9
Josie Knipping <josie.knipping@gmail.com>
2025-05-16 11:26:43 +0200

    the test test_get_my_output works

---
+++ bit_game_2.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+building_a_house_list = [
+    "Paster the walls",
+    "Paint",
+    "Lay the floor",
+    "Tidy up",
+    "Clean",
+    "Look for a couch",
+    "Look for chairs",
+    "Find a TV",
+    "Search for curtains",
+    "Look for paintings",
+    "clean up the garden",
+    "Search for garden furniture",
+    "Clean up the front yard",
+    "Find a desk",
+]
+# print(building_a_house_list)
+
+def get_my_output(levens):
+    bin_string = bin(levens)
+    clean_string = bin_string[2:]
+    turned_string = clean_string[::-1]
+    list_of_elements_binary_string = list(turned_string)
+    list_of_elements_binary_int = list(map(int, list_of_elements_binary_string))
+    gespaarde_elementen = {
+        k: v for k, v in zip(building_a_house_list, list_of_elements_binary_int)
+    }
+
+    return gespaarde_elementen
+
+print(get_my_output(8))
--- tests/test_bit_game.py
+++ tests/test_bit_game.py
@@ -1,3 +1,9 @@
-import os
-from testbook import testbook
-from dotenv import load_dotenv
+# import pytest
+import sys
+sys.path.append('.')
+sys.path.append('./bit_game_2')
+# import bit_game_2
+
+def test_get_my_output():
+    outcome = {'Paster the walls': 0, 'Paint': 0, 'Lay the floor': 0, 'Tidy up': 1}
+    assert bit_game_2.get_my_output(8) == outcome
